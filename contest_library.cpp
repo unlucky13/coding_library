7044b9accf24d5855f719a5b0bed48e460255250
unlucky13 <mazharcse@yahoo.com>
2013-03-20 11:20:19 -0700

    Update contest_library.cpp

--- contest_library.cpp
+++ contest_library.cpp
@@ -78,7 +78,7 @@ return sum;
 
 int Set(int N,int pos){return N=N | (1<<pos);}          /* this will set the bit of the number N at pos to 1 */
 int reset(int N,int pos){return N= N & ~(1<<pos);}      /* this will set the bit of the number N at pos to 0 */
-bool check(int N,int pos){return (bool)(N & (1<<pos));} /*/* this will return the bit of the number N at pos */
+bool check(int N,int pos){return (bool)(N & (1<<pos));} /* this will return the bit of the number N at pos */
 
 
 
