375dbbc4ee93c6872a18e15752bdb82e22c65924
unlucky13 <mazharcse@yahoo.com>
2013-03-20 08:28:33 -0700

    Update contest_library.cpp

--- contest_library.cpp
+++ contest_library.cpp
@@ -80,3 +80,44 @@ int Set(int N,int pos){return N=N | (1<<pos);}          /* this will set the bit
 int reset(int N,int pos){return N= N & ~(1<<pos);}      /* this will set the bit of the number N at pos to 0 */
 bool check(int N,int pos){return (bool)(N & (1<<pos));} /*/* this will return the bit of the number N at pos */
 
+
+
+
+
+/*                     MST - From http://www.shafaetsplanet.com
+========================================================================================================================*/
+
+struct edge
+{
+    int u,v,w;
+    bool operator < ( const edge& p ) const
+    {      
+        return w < p.w;   
+    }
+};
+int par[MAXN];
+vector<edge>e;
+int find(int r)
+{
+   return (par[r]==r) ? r:  find(par[r]);
+}
+int mst(int n)
+{
+    sort(e.begin(),e.end());
+    for(int i=1;i<=n;i++)par[i]=i;
+     
+    int count=0,s=0;
+    for(int i=0;i<(int)e.size();i++)
+    {
+        int u=find(e[i].u);
+        int v=find(e[i].v);
+        if(u!=v)
+        {
+            par[u]=v;
+            count++;
+            s+=e[i].w;
+            if(count==n-1) break;
+        }
+    }
+    return s;
+}
